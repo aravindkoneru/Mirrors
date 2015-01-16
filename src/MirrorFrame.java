d9f7c3ebc8305c660609f868596471849db985e2
Aravind Koneru <aravind.b.koneru@gmail.com>
2015-01-15 21:38:35 -0500

    Completed Diagram
    Did the physics diagram

--- src/MirrorFrame.java
+++ src/MirrorFrame.java
@@ -1,11 +1,38 @@
+import java.awt.event.MouseEvent;
+
 import javax.swing.JFrame;
+import javax.swing.event.MouseInputAdapter;
 
 public class MirrorFrame extends JFrame {
 	
+	MirrorPanel panel = new MirrorPanel(30);
+	
 	public MirrorFrame(){
-		MirrorPanel panel = new MirrorPanel(30);
+		setListener();
 		getContentPane().add(panel);
 		pack();
+	}
+	
+class mouseListener extends MouseInputAdapter {
+		
+		public void mouseDragged(MouseEvent e){
+			int xPos = e.getX();
+			int yPos = e.getY();
+		//	System.out.println(xPos + " " + yPos);
+			panel.setAngle(yPos);
+		}
+		
+		public void mousePressed(MouseEvent e){
+			//System.out.println("pressed");
+			mouseDragged(e);
+			
+		}
+	}
+	
+	public void setListener(){
+		mouseListener listen = new mouseListener();
+		panel.addMouseListener(listen);
+		
 	}
 
 }
--- src/MirrorPanel.java
+++ src/MirrorPanel.java
@@ -1,45 +1,84 @@
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Stroke;
+import java.awt.event.MouseEvent;
 
 import javax.swing.JPanel;
+import javax.swing.event.MouseInputAdapter;
 
 public class MirrorPanel extends JPanel {
 	
 	private double angle = 0;
+	private int yC = 0;
+	private double refAngle = 0;
+	private int space = 0;
+	private boolean ball = false;
 	
 	public MirrorPanel(double ang){
 		angle = ang;
 	}
 	
 	public void paintComponent(Graphics g){
+		
 		Graphics2D g2 = (Graphics2D) g;
+		super.paintComponent(g);
 		
 		//mirror
-		g2.setColor(Color.GRAY);
+		g2.setColor(new Color(52, 73, 94));
 		g2.fillRect(400, 0, 100, 750);
 		
 		//baseline
-		g2.setColor(Color.red);
-		g2.drawLine(0, 500, 400, 500);
+		g2.setStroke(new BasicStroke(2));
+		g2.setColor(Color.black);
+		//g2.drawLine(0, 500, 400, 500);
 		
 		updateLine(g);
+		drawReflection(g);
 		
 	}
 	
-	public void setAngle(double ang){
-		angle = ang;
+	public void setAngle(double yPos){
+		if(yPos < yC){
+			angle += 5;
+		} else if(yPos > yC){
+			angle -= 5;
+		}
+		System.out.println("Angle: " + angle);
+		repaint();
 	}
 	
+	
 	public void updateLine(Graphics g){
+		
 		Graphics2D g2 = (Graphics2D) g;
 		
 		//draw the line
-		int yPos = (int)(Math.tan(Math.toRadians(angle)) * 400);
-		System.out.println(yPos);
-		g2.drawLine(0, 500, 400, yPos);
-		
+		g2.setColor(new Color(231, 76, 60));
+		int yPos = (int)(.3*(Math.tan(Math.toRadians(angle)) * 400));
+		//System.out.println(yPos);
+		final float dash1[] = {10.0f};
+		g2.setStroke(new BasicStroke(3));
+		g2.drawLine(0, 500, 400, 500 - yPos);
+		g2.setColor(Color.BLACK);
+		Stroke dashed = new BasicStroke(3, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[]{9}, 0);
+		g2.setStroke(dashed);
+		g2.drawLine(0, 500 - yPos, 400, 500 - yPos);
+		space = (500 - 2*yPos);
+		yC = 500 - yPos;
 	}
+	
+	public void drawReflection(Graphics g){
+		Graphics2D g2 = (Graphics2D) g;
+		double alpha = 90 / (90 - angle);
+		g2.setColor(new Color(231, 76, 60));
+		g2.setStroke(new BasicStroke(3));
+		g2.drawLine(0, space, 400, yC);
+		g2.setColor(Color.RED);
+	}
+	
+	
 	
 	
 	
